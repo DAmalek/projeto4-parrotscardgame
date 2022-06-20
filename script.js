f6874cf2e8883df74ddad5c908d0caa24c74e416
Daniel <danielmalek101@gmail.com>
2022-06-20 17:16:28 -0300

    ...

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+let identificador = [1,1,2,2,3,3,4,4,5,5,6,6,7,7]; 
+const cartas = document.querySelectorAll("div.card");
+
 function userBurro(){
     x = prompt("escolha quantas cartas quer jogar, de 4 a 14")
+
     if (x%2 !== 0){
         alert("burro tenta de novo");
         userBurro();
@@ -10,8 +14,13 @@ function userBurro(){
         alert("burro tenta de novo");
         userBurro();
     }
+    for (i=0;i<x;i++){
+        cartas[i].classList.add("visivel")
+    }
 }
 userBurro()
+
+
 
 function virada(elemento){
     const virado = elemento.classList.contains("visible");
@@ -21,7 +30,24 @@ function virada(elemento){
     } else {
         elemento.classList.add("visible")
     }
-    
+}
 
+function Dgame(){
+    //fazer com que as cartas iguais fiquem viradas, e diferentes não.
+    let cartasViradas = document.querySelectorAll(".visible")
+    if (cartasViradas[0].querySelector("img .visible").src == cartasViradas[0].querySelector("img .visible").src) {
+        console.log("ok")
+
+    }
+
+    
+}
+
+function desvirador(){
+    
+    for (i=0;i<cartas.length;i++){
+        cartas[i].classList.remove("visible")
+    }
+    desvirador()
 
 }
